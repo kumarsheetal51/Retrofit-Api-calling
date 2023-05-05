23bb89ad9bbaf378fbc5b4ab2bd85a189e51368b
kumarsheetal51 <kumarsheetal51@gmail.com>
2023-05-05 15:58:29 +0530

    add more

---
+++ lib/showdata.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ShowData extends StatefulWidget {
+  const ShowData({Key? key}) : super(key: key);
+
+  @override
+  State<ShowData> createState() => _ShowDataState();
+}
+
+class _ShowDataState extends State<ShowData> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
